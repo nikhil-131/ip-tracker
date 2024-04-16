bf1c673908b705097d420f57dedb949af0c587af
Nikhil R <107304463+nikhil-131@users.noreply.github.com>
2024-04-16 19:25:59 +0530

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -7,47 +7,46 @@ import fetch from "node-fetch"
 const app = express();
 // const port = 3000;
 
-app.use(cors());
+// app.use(cors());
 
 app.use(cors(
     {
-        origin: ["https://ip-tracker-topaz.vercel.app/"],
+        origin: ["https://ip-address-tracker-backend.vercel.app/"],
         methods: ["GET", "POST"]
     }
 ));
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ip-tracker-topaz.vercel.app/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 var data;
-// const fetchData = async () => {
-//     let api = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_u9ZnWhe2bJYyc8piTVt897VBMFoPM&ipAddress=8.8.8.8");
-//     let r = await api.json();
-//     data = r;
-//     console.log(data);
-// }
-// fetchData().then(() => {
-// });
+const fetchData = async (ipAddress) => {
+    // let userIP = await axios.get("https://api.ipify.org?format=json");
+    // console.log("User IP:", userIP.data);
+    let url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_K8ummtQMmIWJok5xIPn4rJqwJMeB9&ipAddress=" + ipAddress;
+    let api = await fetch(url);
+    let r = await api.json();
+    data = r;
+    console.log(data);
+    return data;
+}
 
 app.get("/", async (req, res) => {
     console.log(req.query.data)
-    let api = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_K8ummtQMmIWJok5xIPn4rJqwJMeB9&ipAddress=${req.query.data}`);
-    let r = await api.json();
-    data = r;
+    // let url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_K8ummtQMmIWJok5xIPn4rJqwJMeB9&ipAddress=" + toString(req.query.data);
+    // let api = await fetch(url);
+    // let r = await api.json();
+    // data = r;
+
+    // data = "https://geo.ipify.org/api/v2/country,city?apiKey=at_K8ummtQMmIWJok5xIPn4rJqwJMeB9&ipAddress=" + `${req.query.data}`;
     console.log(data)
     res.json(data);
 })
 
-app.post("/", (req, res) => {
-    console.log(req.body);
-    res.json({ message: "Data received successfully!" });
+app.post("/", async (req, res) => {
+    console.log(req.body.userIP);
+    let ipAddress = req.body.userIP;
+    let ipDetails = await fetchData(ipAddress);
+    res.json(ipDetails);
 })
 
 // app.listen(port, () => {
