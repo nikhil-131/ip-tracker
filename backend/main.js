ee90f853d4e6d7563eea1c9d1272c3513901f5e1
Nikhil R <107304463+nikhil-131@users.noreply.github.com>
2024-05-06 17:49:41 +0530

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -43,15 +43,6 @@ app.get("/", async (req, res) => {
 })
 
 app.post("/", async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
     console.log(req.body.userIP);
     let ipAddress = req.body.userIP;
     let ipDetails = await fetchData(ipAddress);
